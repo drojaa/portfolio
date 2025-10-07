8a2865788a7eb7140c449e184c6d111b37ddf3d9
drojaa <droja@bu.edu>
2025-10-07 03:44:42 -0400

    Mouse + Animations

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -6,11 +6,25 @@ import { Nav } from "../../components/Nav/Nav"
 import { Links } from "../../components/Links/links"
 import { Experience } from "../../components/Experience/Experience"
 
+import { useEffect, useState } from "react"
 export function Home() {
+const [pos, setPos] = useState({ x:0, y:0}) //storing the mouse coordinates
+
+useEffect(() => {
+    function handleMove(e){
+        setPos({x: e.clientX, y: e.clientY})
+
+        document.documentElement.style.setProperty("--mouseX", e.clientX + "px");
+        document.documentElement.style.setProperty("--mouseY", e.clientY + "px");
+    }
+    window.addEventListener("mousemove", handleMove)
+}, [])
+    
     return (
         <>
         <div id="home-container">
         <div>
+        
          <Name/>
          <Nav/>
          <Links/>
@@ -23,6 +37,7 @@ export function Home() {
 
         
         </div>
+        <div class="pointer" aria-hidden="true"></div>
         </>
     )
 }
