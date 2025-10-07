fbaa1543531fa3e67751f3d92629592640c60442
drojaa <droja@bu.edu>
2025-10-07 12:04:50 -0400

    title fadein/out

--- src/components/Name/Name.jsx
+++ src/components/Name/Name.jsx
@@ -1,17 +1,25 @@
 
+import { useEffect, useState } from "react"
 import "./Name.css"
 
 
 export function Name() {
-    const words = ["Entry-level Software Engineer", "Recent Computer Science Graduate"]
+    const words = ["Entry-level Software Engineer", "Recent Computer Science Graduate"];
+    const [current, setCurrent] = useState(0);
+
+    useEffect(() => {
+        const interval = setInterval(() => {
+            setCurrent(prev => (prev + 1) % words.length); 
+          }, 3000); 
+      
+          return () => clearInterval(interval); 
+    }, [])
     return (
         <>
     <div id="name"> 
         <h1 id="title">Derinell Rojas</h1>
 
-        <p>Entry-level Software Engineer <br/>
-        Recent Computer Science Graduate <br/> 
-        </p>
+        <p class="reveal">{words[current]}</p>
 
        <p id="desc"> Solving technical challenges through user-focused,<br/>
        polished digital solutions</p>
