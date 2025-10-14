84230e9ce379313accfad26d072ecb13622ca1af
drojaa <droja@bu.edu>
2025-10-14 17:45:48 -0400

    added projects

--- src/components/Portfolio/Portfolio.jsx
+++ src/components/Portfolio/Portfolio.jsx
@@ -15,14 +15,41 @@ export function Portfolio() {
           <a href="https://cs412-9c2s.onrender.com/voter_analytics/" > 
 
           <p id="image">
+          <img src="src/public/two.png"></img>
+              </p>
+             
+                <p id="port-desc"> <span>Bookify <PiCursorClickDuotone /> </span> <br></br>
+                A web app that generates personalized Spotify playlists from book or movie selections. Users enter a title and author, and OpenAI’s ChatGPT creates a playlist matching the work’s themes and vibe. Playlists are saved to the user’s Spotify account. Users can rate and comment, fostering community engagement. Built with Flask, React, Tailwind CSS, Spotify API, and OpenAI integration.
+                </p>
+          </a>
+              
+            </div>
+
+            <div id="portfolio" class="main">
+          <a href="https://cs412-9c2s.onrender.com/voter_analytics/" > 
+
+          <p id="image">
           <img src="src/public/one.png"></img>
               </p>
              
                 <p id="port-desc"> <span>Voter Analytics  <PiCursorClickDuotone /> </span> <br></br>
                 An interactive dashboard for Newton, MA voter data. Users can browse, filter, and paginate through voter records, and analyze data via bar and pie charts to gain insights into demographics and election trends. Built with Django, React (JS), and CSS.
                 </p>
-          </a>
-              
+          </a>   
+            </div>
+
+
+            <div id="portfolio" class="main">
+          <a href="https://cs412-9c2s.onrender.com/voter_analytics/" > 
+
+          <p id="image">
+          <img src="src/public/three.png"></img>
+              </p>
+             
+                <p id="port-desc"> <span>Radiology Report<PiCursorClickDuotone /> </span> <br></br>
+                An interactive dashboard for Newton, MA voter data. Users can browse, filter, and paginate through voter records, and analyze data via bar and pie charts to gain insights into demographics and election trends. Built with Django, React (JS), and CSS.
+                </p>
+          </a>   
             </div>
 
         </div>
