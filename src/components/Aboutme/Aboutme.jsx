f56f9edf962f693c9a8935cc3fe0a03e8403414c
drojaa <droja@bu.edu>
2025-10-19 12:19:13 -0400

    about section completed

--- src/components/Aboutme/Aboutme.jsx
+++ src/components/Aboutme/Aboutme.jsx
@@ -5,13 +5,19 @@ export function Aboutme() {
     return (
         <>
         <div id="about-container">
-        <p class="about"> Hey there! I’m Derinell Rojas, a self-driven undergrad visionary who loves blending creativity and logic to produce innovative solutions that are both visually engaging and functionally meaningful. I’m pursuing a Bachelor of Arts in Computer Science at <span>Boston University</span>, supported by a broad foundation in sociology, art history, journalism, and marketing/advertising, built through both coursework and external experiences.</p>
-    
-        <p class="about"> These interdisciplinary pursuits have given me a holistic perspective, empowering me to approach projects thoughtfully, creating work that is socially conscious, artistically rich, and strategically crafted. My career ambitions include becoming a Software Engineer and eventually advancing to a Senior level. I also plan to pursue a Master’s degree in Software Engineering, Software Development, or potentially Digital Forensics. </p>
-      
-        <p class="about"> In my free time, I actively <span>volunteer</span> at tech-related events, recognizing firsthand how mentorship, workshops, and hands-on experiences can profoundly shape personal and professional development. </p>
-        
-   
+        <p class="about">
+  Hey there! I see technology as the meeting point between creativity and logic , a space where ideas become tools that shape the future and simplify everyday life. 
+  I earned my Bachelor of Arts in Computer Science from Boston University, where I explored how technology bridges industries through hands-on work in marketing, healthtech, and public safety. I also volunteered at tech conferences, hackathons, and national tech organizations to engage with the tech community, stay inspired by emerging trends, and contribute to inclusive spaces for innovation. 
+  Those experiences deepened my belief that software has the power to connect people and create lasting impact. 
+  <br></br><br></br>
+  I’m excited to keep growing as a Software Engineer and eventually pursue a Master’s in Software Development or Digital Forensics to continue expanding my craft. 
+  <br></br><br></br>In my free time, I enjoy reading psychological mysteries, baking for loved ones, and finding inspiration through art.
+</p>
+
+<p class="about">
+  Feel free to explore my webpage, and thank you for taking the time to visit!
+</p>
+
         </div>
         
         </>
