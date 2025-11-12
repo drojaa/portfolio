624650e78e7c53b6e8c1a6131ae5411e6c5c7fa1
Derinell Rojas <119087348+drojaa@users.noreply.github.com>
2025-11-12 00:40:55 -0500

    Update AboutMe component with revised text
    Revised personal introduction and experiences for clarity and impact.

--- src/components/Aboutme/Aboutme.jsx
+++ src/components/Aboutme/Aboutme.jsx
@@ -6,12 +6,12 @@ export function Aboutme() {
         <>
         <div id="about-container">
         <p class="about">
-  Hey there! I see technology as the meeting point between creativity and logic , a space where ideas become tools that shape the future and simplify everyday life. 
-  I earned my Bachelor of Arts in Computer Science from Boston University, where I explored how technology bridges industries through hands-on work in marketing, healthtech, and public safety. I also volunteered at tech conferences, hackathons, and national tech organizations to engage with the tech community, stay inspired by emerging trends, and contribute to inclusive spaces for innovation. 
-  Those experiences deepened my belief that software has the power to connect people and create lasting impact. 
+Hey there! I see technology as the meeting point between creativity and logic, a space where ideas become tools and those tools become solutions that drive progress.
+As I complete my Bachelor of Arts in Computer Science at Boston University, I’ve begun to explore how technology bridges different industries through hands-on work in public safety, healthtech, and marketing. I’ve also volunteered at tech conferences, hackathons, and national tech organizations to engage with the tech community, stay inspired by emerging trends, and contribute to inclusive spaces for innovation.
+These experiences have deepened my belief that software has the power to connect people and create lasting impact.
   <br></br><br></br>
-  I’m excited to keep growing as a Software Engineer and eventually pursue a Master’s in Software Development or Digital Forensics to continue expanding my craft. 
-  <br></br><br></br>In my free time, I enjoy reading psychological mysteries, baking for loved ones, and finding inspiration through art.
+  I’m committed to continuously developing my technical skills and knowledge as I grow into roles that challenge and inspire me.
+  <br></br><br></br>When I’m not immersed in tech, I love reading mystery books, solving Sudoku puzzles, baking for loved ones, and finding inspiration through art.
 </p>
 
 <p class="about">
